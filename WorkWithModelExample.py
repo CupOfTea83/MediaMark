279adb0d1fcf8bed5bce3329e8abbb6f526ea0d5
Рябов Никита <riabovnick080@yandex.ru>
2024-12-01 16:30:28 +0500

    add example script

---
+++ WorkWithModelExample.py
@@ -0,0 +1,9 @@
+from Model import Model
+model = Model()
+score = model.GetScore(name = "",
+                       genres = ["","",""],
+                       media_type = "",
+                       episodes = 0,
+                       date = 0,
+                       description = "")
+                       
