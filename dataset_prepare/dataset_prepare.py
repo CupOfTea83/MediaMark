32c7a411dea881907304b2bd00f21ec5d40de072
CupOfTea83 <62793924+CupOfTea83@users.noreply.github.com>
2024-12-09 22:33:26 +0500

    Исправлено создание матрицы признаков.

--- dataset_prepare/dataset_prepare.py
+++ dataset_prepare/dataset_prepare.py
@@ -29,12 +29,12 @@ for index, row in dataset_merged.iterrows():
 dataset_merged = dataset_merged.merge(pandas.DataFrame(year_aired), how='inner', on="MAL_ID")
 
 name_vectorizer = CountVectorizer()
-name_vectors = name_vectorizer.fit_transform(dataset_merged["Name"].values.astype('U')).toarray()
+name_vectors = name_vectorizer.fit_transform(dataset_merged["Name"].values.astype('U'))
 print(name_vectors.shape)
 joblib.dump(name_vectorizer, 'name_vectorizer.joblib', compress=9)
 
 description_vectorizer = CountVectorizer()
-description_vectors = description_vectorizer.fit_transform(dataset_merged["sypnopsis"].values.astype('U')).toarray()
+description_vectors = description_vectorizer.fit_transform(dataset_merged["sypnopsis"].values.astype('U'))
 print(description_vectors.shape)
 joblib.dump(description_vectorizer, 'description_vectorizer.joblib', compress=9)
 
@@ -43,6 +43,6 @@ dict_vectors = dict_vectorizer.fit_transform(dataset_no_text_dicts).toarray()
 print(dict_vectors.shape)
 joblib.dump(dict_vectorizer, 'dict_vectorizer.joblib', compress=9)
 
-all_vectors = sps.hstack([name_vectors, description_vectors, dict_vectors]).toarray()
+all_vectors = sps.hstack([name_vectors, description_vectors, dict_vectors]).todense()
 print(type(all_vectors))
 joblib.dump(all_vectors, 'vectors_np.joblib', compress=9)
