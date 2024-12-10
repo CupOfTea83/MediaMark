fe207117504143538c314e22d2cf656fb71e4e5a
Рябов Никита <riabovnick080@yandex.ru>
2024-12-10 17:47:19 +0500

    trainer is ready

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import numpy as np
-import pandas as pd
 import joblib
 
 PATH_X = "C:\\Users\\Никита\\Desktop\\data\\vectors_np.joblib"
@@ -9,8 +10,8 @@ PATH_Y = "C:\\Users\\Никита\\Desktop\\data\\scores_np.joblib"
 PATH_MODEL = ".\\model.joblib"
 
 print("Loading data - ", end="")
-x = joblib.load(PATH_X)
-y = joblib.load(PATH_Y)
+x = np.asarray(joblib.load(PATH_X))
+y = np.asarray(joblib.load(PATH_Y))
 print("Done")
 
 print("Preparing train/test data - ", end="")
@@ -25,7 +26,7 @@ print("Learning model - ", end="")
 model.fit(x_train, y_train)
 print("Done")
 
-score = model.score(x_test, y_test)
+score = sqrt(mean_squared_error(model.predict(x_test), y_test))
 print("Score -", score)
 
 print("Save? [y/n]")
