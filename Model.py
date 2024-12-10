6fcd353c56fa32c271c4ed675af765f1f5f61dd5
Рябов Никита <riabovnick080@yandex.ru>
2024-12-10 17:55:28 +0500

    using vectorizer

--- Model.py
+++ Model.py
@@ -1,16 +1,22 @@
-import pickle
+import joblib
+from sklearn.neural_network import MLPRegressor
+from vectorization_module.vectorization import vectorize_parameters
 
 class Model():
-    _MODEL_PATH = "./model.dat"
+    _MODEL_PATH = "./model.joblib"
     
     def __init__():
-        with open(self._MODEL_PATH, 'rb') as file:
-            self.model = pickle.load(file)
+        self.model = joblib.load(_MODEL_PATH)
 
     def GetScore(**kwargs):
-
-        #TODO vectorization
-        parameters = []
+        parameters = vectorize_parameters(kwargs["name"],
+                         kwargs["description"],
+                         kwargs["genres"],
+                         kwargs["media_type"],
+                         kwargs["episodes"],
+                         kwargs["year"],
+                         kwargs["studios"],
+                         kwargs["rating"])
 
         score = self.model.predict([parameters])
 
