b1a12fd1d2b2594ce472b79531856cd384e7dfb8
Артём Азмагулов <azmagulov95@bk.ru>
2024-12-13 18:42:06 +0500

    Изменение расположения кнопок и добавление фона

--- interface.py
+++ interface.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import test2
 class Example(QMainWindow, test2.Ui_MainWindow):
+    
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -42,9 +43,38 @@ class Example(QMainWindow, test2.Ui_MainWindow):
         return ''.join(random.choice(chars) for x in range(size))
     def buttonClicked(self):
         self.textEdit.append(self.generate_pins(10))
-    
+stylesheet = """
+    QMainWindow {
+        background-image: url("fon1.jpg");
+        background-repeat: no-repeat; 
+        background-position: center;
+    }
+    QLabel {
+        font: 20pt Segoe UI Semibold;
+    }
+    QLabel#label {
+        font: 20pt Segoe UI Semibold;
+        background-color: #ddfff7;
+    }
+    QPushButton {
+        font: 15pt Segoe UI Semibold;
+    }
+    QComboBox {
+        font: 10pt Segoe UI Semibold;
+    }
+    QLineEdit {
+        font: 10pt Segoe UI Semibold;
+    }
+    QDateEdit {
+        font: 10pt Segoe UI Semibold;
+    }
+    QCheckBox {
+        font: 10pt Segoe UI Semibold;
+    }
+"""    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyleSheet(stylesheet)
     form = Example()
     form.show()
     app.exec()
