423ac7d7287f846427ff6670a74b18ad2aba400f
Рябов Никита <riabovnick080@yandex.ru>
2024-12-14 18:41:40 +0500

    bug fix: data dont write

--- ImportExport.py
+++ ImportExport.py
@@ -14,14 +14,14 @@ def _GetVersion():
 def Export(path = "./file.json", **kwargs):
     metadata = {}
     
-    metadata["time"] = datetime.datetime.now()
+    metadata["time"] = str(datetime.datetime.now())
     metadata["version"] = _GetVersion()
 
     kwargs["metadata"] = metadata
-
+    
     try:
         with open(path, "w") as file:
-            json.dump(file, kwargs)
+            json.dump(kwargs, file)
         return True
     except Exception:
         return False
@@ -32,5 +32,18 @@ def Import(path):
             data = json.load(file)
         return data
     except Exception:
-        pass
+        return False
+
+if __name__ == "__main__":
+    Export(path = "./file.json",
+           name = "NAME",
+           description = "DESCRIPTION",
+           genres = ["GENRE1","GENRE2"], 
+           media_type = "TV",
+           episodes = 12,
+           year = 2024,
+           studios = ["STUDIO1","STUDIO2"],
+           rating = "R")
+    data = Import(path = "./file.json")
+    print(data)
     
